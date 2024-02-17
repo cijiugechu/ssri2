0f0b1b62f1fae73be6ce1dc727d194b77fb9b558
cijiugechu <cijiugechu@gmail.com>
2024-02-17 15:41:16 +0800

    chore: format

--- benches/integrity.rs
+++ benches/integrity.rs
@@ -2,12 +2,12 @@ use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use ssri2::Integrity;
 
 const SOURCE: &str = "sha256-uU0nuZNNPgilLlLX2n2r+sSE7+N6U4DukIj3rOLvzek=";
-const TEXT: &[u8]  = b"hello world";
+const TEXT: &[u8] = b"hello world";
 
 pub fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("parse", |b| {
         b.iter(|| {
-          let _ = Integrity::from(black_box(SOURCE));
+            let _ = Integrity::from(black_box(SOURCE));
         })
     });
 
