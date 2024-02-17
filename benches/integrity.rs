f60feaca450cea6717feda032934446ad6c830cd
cijiugechu <cijiugechu@gmail.com>
2024-02-17 15:54:04 +0800

    chore: bench concat

--- benches/integrity.rs
+++ benches/integrity.rs
@@ -3,6 +3,8 @@ use ssri2::Integrity;
 
 const SOURCE: &str = "sha256-uU0nuZNNPgilLlLX2n2r+sSE7+N6U4DukIj3rOLvzek=";
 const TEXT: &[u8] = b"hello world";
+const SEG1: &[u8] = b"sha256-deadbeef";
+const SEG2: &[u8] = b"sha256-badc0ffee";
 
 pub fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("parse", |b| {
@@ -15,6 +17,14 @@ pub fn criterion_benchmark(c: &mut Criterion) {
         b.iter(|| {
             let sri = Integrity::from(black_box(TEXT));
             let _ = sri.check(black_box(TEXT)).unwrap();
+        })
+    });
+
+    c.bench_function("concat", |b| {
+        b.iter(|| {
+            let sri1 = Integrity::from(black_box(SEG1));
+            let sri2 = Integrity::from(black_box(SEG2));
+            let _ = sri1.concat(black_box(sri2));
         })
     });
 }
